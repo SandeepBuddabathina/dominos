d588897382c5fedc7e237ac00ba916e9bb94a291
Sumanth0005 <sumanthkurapati13@gmail.com>
2025-04-23 10:23:48 +0530

    changes

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -48,10 +48,7 @@ export class CartComponent implements OnInit {
   }
   
   // Method to add item to the cart with dynamic image
-  addToCart(item: any): void {
-    this.cartService.addItem(item);
-    this.updateCart();
-  }
+
 
   getUserLocation(): void {
     this.locationService.getLocation().then((coords: any) => {
